8c40be8135dc172551e05ba80815629b946a0a45
saltandbeer <raditude28@gmail.com>
2013-10-12 11:41:14 -0400

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -43,6 +44,12 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to root_path, notice: "Please sign in." unless signed_in?
+  end
+
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
